2ce4ddb332f16637bc362a0c7ab7c427e9f76a2e
AcePilot10 <codyjg10@gmail.com>
2017-10-10 17:48:56 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CDController
+{
+    partial class CDControllerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCurrent);
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(22, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Disk Tray Controls";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(19, 78);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 30);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(144, 78);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 30);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCurrent.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrent.Location = new System.Drawing.Point(49, 162);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(164, 26);
+            this.lblCurrent.TabIndex = 3;
+            this.lblCurrent.Text = "CD Is Currently Closed";
+            this.lblCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CDControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 249);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CDControllerForm";
+            this.Text = "CD Controller";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblCurrent;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CDController
+{
+    public partial class CDControllerForm : Form
+    {
+   
+        //Media Control Interface (MCI)
+        //Imports and registers the function for an MCI command
+        [DllImport("winmm.dll")]
+        protected static extern int mciSendString(string cmd, StringBuilder returnString, int returnLength, IntPtr callBack);
+      /* Parameters:
+       * 
+       * cmd = Command to be ran
+       * returnString = returns information about command
+       * returnLength = Size, in characters of the return buffer of returnString
+       * callBack = Handles to a callback window
+       * 
+       */
+
+
+        public CDControllerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            mciSendString("set cdaudio door open", null, 0, IntPtr.Zero);
+            lblCurrent.Text = "CD Is Currently Open";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            mciSendString("set cdaudio door closed", null, 0, IntPtr.Zero);
+            lblCurrent.Text = "CD Is Currently Closed";
+        }
+    }
+}
